5bd59d497da4060582cb458e436fb1fe6e96225d
SashoAlex2000 <111345560+SashoAlex2000@users.noreply.github.com>
2022-09-29 12:36:06 +0300

    Update ranking_old.py

--- dictionaries/ranking_old.py
+++ dictionaries/ranking_old.py
@@ -44,3 +44,38 @@ for user in sorted(user_dict.keys()):
     for contest,points in sorted(user_dict[user].items(), key = lambda cp: -cp[1]):
         print(f"#  {contest} -> {user_dict[user][contest]}")
 
+
+        
+# Test Input:  
+#     #1
+# Part One Interview:success
+# JS Fundamentals:fundExam
+# C# Fundamentals:fundPass
+# Algorithms:fun
+# end of contests
+# C# Fundamentals=>fundPass=>Tanya=>350
+# Algorithms=>fun=>Tanya=>380
+# Part One Interview=>success=>Nikola=>120
+# Java Basics Exam=>wrong_pass=>Teo=>400
+# Part One Interview=>success=>Tanya=>220
+# OOP Advanced=>password123=>Kay=>231
+# C# Fundamentals=>fundPass=>Tanya=>250
+# C# Fundamentals=>fundPass=>Nikola=>200
+# JS Fundamentals=>fundExam=>Tanya=>400
+# end of submissions
+
+#     #2
+# Java Advanced:funpass
+# Part Two Interview:success
+# Math Concept:asdasd
+# Java Web Basics:forrF
+# end of contests
+# Math Concept=>ispass=>Monika=>290
+# Java Advanced=>funpass=>Simona=>400
+# Part Two Interview=>success=>Drago=>120
+# Java Advanced=>funpass=>Petyr=>90
+# Java Web Basics=>forrF=>Simona=>280
+# Part Two Interview=>success=>Petyr=>0
+# Math Concept=>asdasd=>Drago=>250
+# Part Two Interview=>success=>Simona=>200
+# end of submissions
